ded5eab074f361d4b0040107c30dcd1795ee532a
IagoLast <iago.lastra@gmail.com>
2026-01-01 18:38:07 +0100

    fix: oauth

--- back/src/modules/auth/services/google-auth.service.ts
+++ back/src/modules/auth/services/google-auth.service.ts
@@ -27,8 +27,7 @@ export class GoogleAuthService {
   ) {
     this.client = new OAuth2Client(
       process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      'postmessage'
+      process.env.GOOGLE_CLIENT_SECRET
     );
   }
 
